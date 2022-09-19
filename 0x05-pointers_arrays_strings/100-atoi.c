2584176e6ec0cc7b4552b0269c98882b551a619c
Wavinyaa <mitchellewavinya@gmail.com>
2022-09-19 14:19:07 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,14 +15,15 @@ n = 0;
 len = 0;
 f = 0;
 digit = 0;
-while (s[len] != 0)
+while (s[len] != '\0')
 len++;
 while (i < len && f == 0)
 {
 if (s[i] == '-')
 ++d;
-}
+
 if (s[i] >= 0 && s[i] <= 9)
+{
 digit = s[i] - 0;
 if (d % 2)
 digit = -digit;
