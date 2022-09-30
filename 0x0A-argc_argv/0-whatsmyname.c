e3d2f662d8cfb904e3e53c19d11a8f1603bc76a1
Wavinyaa <mitchellewavinya@gmail.com>
2022-09-30 14:42:41 -0700

    first argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument no
+ * @argv: array of arg
+ *
+ * Return:0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
